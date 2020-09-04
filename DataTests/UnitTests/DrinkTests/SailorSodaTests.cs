da0cd42019432baf429e07e33ca7c5f9a75eccc7
Christian Moden <ctmoden@ksu.edu>
2020-09-04 15:27:06 -0500

    having problems with peach to string in soda class

--- DataTests/UnitTests/DrinkTests/SailorSodaTests.cs
+++ DataTests/UnitTests/DrinkTests/SailorSodaTests.cs
@@ -51,7 +51,11 @@ namespace BleakwindBuffet.DataTests.UnitTests.DrinkTests
         [Fact]
         public void ShouldBeAbleToSetSize()
         {
-            
+            SailorSoda soda = new SailorSoda();
+            soda.Ice = true;
+            Assert.True(soda.Ice);
+            soda.Ice = false;
+            Assert.False(soda.Ice);
         }
 
         [Fact]
@@ -69,56 +73,107 @@ namespace BleakwindBuffet.DataTests.UnitTests.DrinkTests
             soda.Flavor = SodaFlavor.Peach;
             Assert.Equal(SodaFlavor.Peach, soda.Flavor);
         }
-
+        
         [Theory]
+        /// <summary>
+        /// Size small should cost 1.42
+        /// </summary>
         [InlineData(Size.Small, 1.42)]
+        /// <summary>
+        /// Size medium should cost 1.74
+        /// </summary>
         [InlineData(Size.Medium, 1.74)]
+        /// <summary>
+        /// Size large should cost 2.07
+        /// </summary>
         [InlineData(Size.Large, 2.07)]
         public void ShouldHaveCorrectPriceForSize(Size size, double price)
         {
+            SailorSoda soda = new SailorSoda();
+            soda.Size = size;
+            Assert.Equal(price, soda.Price);
         }
 
         [Theory]
+        /// <summary>
+        /// Size small should have 117 calories
+        /// </summary>
         [InlineData(Size.Small, 117)]
+        /// <summary>
+        /// Size small should have 153 calories
+        /// </summary>
         [InlineData(Size.Medium, 153)]
+        /// <summary>
+        /// Size small should have 205 calories
+        /// </summary>
         [InlineData(Size.Large, 205)]
         public void ShouldHaveCorrectCaloriesForSize(Size size, uint cal)
         {
+            SailorSoda soda = new SailorSoda();
+            soda.Size = size;
+            Assert.Equal(cal, soda.Calories);
         }
 
         [Theory]
+        /// <summary>
+        /// ice included
+        /// </summary>
         [InlineData(true)]
+        /// <summary>
+        /// Ice witheld 
+        /// </summary>
         [InlineData(false)]
         public void ShouldHaveCorrectSpecialInstructions(bool includeIce)
         {
+            SailorSoda soda = new SailorSoda();
+            soda.Ice = includeIce;
+            if (!includeIce) Assert.Contains("Hold ice", soda.SpecialInstructions);
+            if (includeIce) Assert.Empty(soda.SpecialInstructions);
         }
         
         [Theory]
+        /// <summary>
+        /// All sizes for cherry flavor
+        /// </summary>
         [InlineData(SodaFlavor.Cherry, Size.Small, "Small Cherry Sailor Soda")]
         [InlineData(SodaFlavor.Cherry, Size.Medium, "Medium Cherry Sailor Soda")]
         [InlineData(SodaFlavor.Cherry, Size.Large, "Large Cherry Sailor Soda")]
-
+        /// <summary>
+        /// All sizes for blackberry flavor
+        /// </summary>
         [InlineData(SodaFlavor.Blackberry, Size.Small, "Small Blackberry Sailor Soda")]
         [InlineData(SodaFlavor.Blackberry, Size.Medium, "Medium Blackberry Sailor Soda")]
         [InlineData(SodaFlavor.Blackberry, Size.Large, "Large Blackberry Sailor Soda")]
-
+        /// <summary>
+        /// All sizes for grapefruit flavor
+        /// </summary>
         [InlineData(SodaFlavor.Grapefruit, Size.Small, "Small Grapefruit Sailor Soda")]
         [InlineData(SodaFlavor.Grapefruit, Size.Medium, "Medium Grapefruit Sailor Soda")]
         [InlineData(SodaFlavor.Grapefruit, Size.Large, "Large Grapefruit Sailor Soda")]
-
+        /// <summary>
+        /// All sizes for lemon flavor
+        /// </summary>
         [InlineData(SodaFlavor.Lemon, Size.Small, "Small Lemon Sailor Soda")]
         [InlineData(SodaFlavor.Lemon, Size.Medium, "Medium Lemon Sailor Soda")]
         [InlineData(SodaFlavor.Lemon, Size.Large, "Large Lemon Sailor Soda")]
-
-        [InlineData(SodaFlavor.Peach, Size.Small, "Small Peach Soda")]
-        [InlineData(SodaFlavor.Peach, Size.Medium, "Medium Peach Soda")]
-        [InlineData(SodaFlavor.Peach, Size.Large, "Small Peach Soda")]
-
+        /// <summary>
+        /// All sizes for peach flavor
+        /// </summary>
+        [InlineData(SodaFlavor.Peach, Size.Small, "Small Peach Sailor Soda")]
+        [InlineData(SodaFlavor.Peach, Size.Medium, "Medium Peach Sailor Soda")]
+        [InlineData(SodaFlavor.Peach, Size.Large, "Large Peach Sailor Soda")]
+        /// <summary>
+        /// All sizes for watermelon flavor
+        /// </summary>
         [InlineData(SodaFlavor.Watermelon, Size.Small, "Small Watermelon Sailor Soda")]
         [InlineData(SodaFlavor.Watermelon, Size.Medium, "Medium Watermelon Sailor Soda")]
         [InlineData(SodaFlavor.Watermelon, Size.Large, "Large Watermelon Sailor Soda")]
         public void ShouldHaveCorrectToStringBasedOnSizeAndFlavor(SodaFlavor flavor, Size size, string name)
         {
+            SailorSoda soda = new SailorSoda();
+            soda.Flavor = flavor;
+            soda.Size = size;
+            Assert.Equal(name, soda.ToString());
         }
     }
 }
