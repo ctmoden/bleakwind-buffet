80badae1cd018c58bfbb3810ccbf3f2963bcd48e
Christian Moden <ctmoden@ksu.edu>
2020-09-04 22:05:54 -0500

    just created water test

---
+++ DataTests/UnitTests/DrinkTests/WarriorWaterTests.cs
@@ -0,0 +1,23 @@
+﻿
+/*
+ * Author:  Christian Moden
+ * Class: WarriorWaterTests.cs
+ * Purpose: Test the WarriorWater.cs class in the Data library
+ */
+using Xunit;
+
+using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data.Drinks;
+
+namespace BleakwindBuffet.DataTests.UnitTests.DrinkTests
+{
+    public class WarriorWaterTests
+    {
+        [Fact]
+        public void ShouldIncludeIceByDefault()
+        {
+
+        }
+    }
+}
