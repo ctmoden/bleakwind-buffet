f21e73c4e85a3b7d65956e0a7c9b14a509b7aeaa
Christian Moden <ctmoden@ksu.edu>
2020-09-04 20:43:21 -0500

    done with philly

--- DataTests/UnitTests/EntreeTests/PhillyPoacherTests.cs
+++ DataTests/UnitTests/EntreeTests/PhillyPoacherTests.cs
@@ -6,6 +6,8 @@
 using Xunit;
 
 using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Entrees;
+using Microsoft.VisualStudio.TestPlatform.Common.Interfaces;
 
 namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
 {
@@ -14,41 +16,67 @@ namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
         [Fact]
         public void ShouldInlcudeSirloinByDefault()
         {
+            PhillyPoacher philly = new PhillyPoacher();
+            Assert.True(philly.Sirloin);
+
         }
 
         [Fact]
         public void ShouldInlcudeOnionByDefault()
         {
+            PhillyPoacher philly = new PhillyPoacher();
+            Assert.True(philly.Onion);
         }
 
         [Fact]
         public void ShouldInlcudeRollByDefault()
         {
+            PhillyPoacher philly = new PhillyPoacher();
+            Assert.True(philly.Roll);
         }
 
         [Fact]
         public void ShouldBeAbleToSetSirloin()
         {
+            PhillyPoacher philly = new PhillyPoacher();
+            philly.Sirloin = true;
+            Assert.True(philly.Sirloin);
+            philly.Sirloin = false;
+            Assert.False(philly.Sirloin);
         }
 
         [Fact]
         public void ShouldBeAbleToSetOnions()
         {
+            PhillyPoacher philly = new PhillyPoacher();
+            philly.Onion = true;
+            Assert.True(philly.Onion);
+            philly.Onion = false;
+            Assert.False(philly.Onion);
         }
 
         [Fact]
         public void ShouldBeAbleToSetRoll()
         {
+            PhillyPoacher philly = new PhillyPoacher();
+            philly.Roll = true;
+            Assert.True(philly.Roll);
+            philly.Roll = false;
+            Assert.False(philly.Roll);
         }
 
         [Fact]
         public void ShouldReturnCorrectPrice()
         {
+            PhillyPoacher philly = new PhillyPoacher();
+            Assert.Equal(7.23, philly.Price);
         }
 
         [Fact]
         public void ShouldReturnCorrectCalories()
         {
+            PhillyPoacher philly = new PhillyPoacher();
+            Assert.Equal(784, (double)philly.Calories);
         }
 
         [Theory]
@@ -57,11 +85,21 @@ namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
         public void ShouldReturnCorrectSpecialInstructions(bool includeSirloin, bool includeOnion,
                                                             bool includeRoll)
         {
+            PhillyPoacher philly = new PhillyPoacher();
+            philly.Sirloin = includeSirloin;
+            philly.Onion = includeOnion;
+            philly.Roll = includeRoll;
+            if (!includeSirloin) Assert.Contains("Hold sirloin", philly.SpecialInstructions);
+            if (!includeOnion) Assert.Contains("Hold onion", philly.SpecialInstructions);
+            if (!includeRoll) Assert.Contains("Hold roll", philly.SpecialInstructions);
+            if (includeRoll && includeOnion && includeSirloin) Assert.Empty(philly.SpecialInstructions);
         }
 
         [Fact]
         public void ShouldReturnCorrectToString()
         {
+            PhillyPoacher philly = new PhillyPoacher();
+            Assert.Equal("Philly Poacher", philly.ToString());
         }
     }
 }
--- DataTests/UnitTests/EntreeTests/SmokehouseSkeletonTests.cs
+++ DataTests/UnitTests/EntreeTests/SmokehouseSkeletonTests.cs
@@ -6,6 +6,7 @@
 using Xunit;
 
 using BleakwindBuffet.Data;
+using BleakwindBuffet.Data.Entrees;
 
 namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
 {
@@ -14,41 +15,69 @@ namespace BleakwindBuffet.DataTests.UnitTests.EntreeTests
         [Fact]
         public void ShouldInlcudeSausageByDefault()
         {
+            SmokehouseSkeleton smoke = new SmokehouseSkeleton();
+            Assert.True(smoke.SausageLink);
         }
 
         [Fact]
         public void ShouldInlcudeEggByDefault()
         {
+            SmokehouseSkeleton smoke = new SmokehouseSkeleton();
+            Assert.True(smoke.Egg);
         }
 
         [Fact]
         public void ShouldInlcudeHashbrownsByDefault()
         {
+            SmokehouseSkeleton smoke = new SmokehouseSkeleton();
+            Assert.True(smoke.HashBrowns);
         }
 
         [Fact]
         public void ShouldInlcudePancakeByDefault()
         {
+            SmokehouseSkeleton smoke = new SmokehouseSkeleton();
+            Assert.True(smoke.Pancake);
         }
 
         [Fact]
         public void ShouldBeAbleToSetSausage()
         {
+            SmokehouseSkeleton smoke = new SmokehouseSkeleton();
+            smoke.SausageLink = true;
+            Assert.True(smoke.SausageLink);
+            smoke.SausageLink = false;
+            Assert.False(smoke.SausageLink);
         }
 
         [Fact]
         public void ShouldBeAbleToSetEgg()
         {
+            SmokehouseSkeleton smoke = new SmokehouseSkeleton();
+            smoke.Egg = true;
+            Assert.True(smoke.Egg);
+            smoke.Egg = false;
+            Assert.False(smoke.Egg);
         }
 
         [Fact]
         public void ShouldBeAbleToSetHashbrowns()
         {
+            SmokehouseSkeleton smoke = new SmokehouseSkeleton();
+            smoke.HashBrowns = true;
+            Assert.True(smoke.HashBrowns);
+            smoke.HashBrowns = false;
+            Assert.False(smoke.HashBrowns);
         }
 
         [Fact]
         public void ShouldBeAbleToSetPancake()
         {
+            SmokehouseSkeleton smoke = new SmokehouseSkeleton();
+            smoke.Pancake = true;
+            Assert.True(smoke.Pancake);
+            smoke.Pancake = false;
+            Assert.False(smoke.Pancake);
         }
 
         [Fact]
