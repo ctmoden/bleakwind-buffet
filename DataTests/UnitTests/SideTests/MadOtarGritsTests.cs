ff599fa90955681efcc170e23d866a55dc568ae3
Christian Moden <ctmoden@ksu.edu>
2020-09-04 21:54:26 -0500

    grits done

--- DataTests/UnitTests/SideTests/MadOtarGritsTests.cs
+++ DataTests/UnitTests/SideTests/MadOtarGritsTests.cs
@@ -1,5 +1,6 @@
 ﻿/*
  * Author: Zachery Brunner
+ * Modified by: Christian Moden
  * Class: MadOtarGritsTests.cs
  * Purpose: Test the MadOtarGrits.cs class in the Data library
  */
@@ -7,6 +8,7 @@ using Xunit;
 
 using BleakwindBuffet.Data;
 using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data.Sides;
 
 namespace BleakwindBuffet.DataTests.UnitTests.SideTests
 {
@@ -15,40 +17,90 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [Fact]
         public void ShouldBeSmallByDefault()
         {
+            MadOtarGrits grits = new MadOtarGrits();
+            Assert.Equal(Size.Small, grits.Size);
         }
                 
         [Fact]
         public void ShouldBeAbleToSetSize()
         {
+            MadOtarGrits grits = new MadOtarGrits();
+            grits.Size = Size.Small;
+            Assert.Equal(Size.Small, grits.Size);
+            grits.Size = Size.Medium;
+            Assert.Equal(Size.Medium, grits.Size);
+            grits.Size = Size.Large;
+            Assert.Equal(Size.Large, grits.Size);
+            grits.Size = Size.Small;
+            Assert.Equal(Size.Small, grits.Size);
+
         }
 
         [Fact]
         public void ShouldReturnCorrectStringOnSpecialInstructions()
         {
+            MadOtarGrits grits = new MadOtarGrits();
+            Assert.Empty(grits.SpecialInstructions);
         }
 
         [Theory]
+        /// <summary>
+        /// small should cost 1.22
+        /// </summary>
         [InlineData(Size.Small, 1.22)]
+        /// <summary>
+        /// medium should cost 1.58
+        /// </summary>
         [InlineData(Size.Medium, 1.58)]
+        /// <summary>
+        /// large should cost 1.93
+        /// </summary>
         [InlineData(Size.Large, 1.93)]
         public void ShouldReturnCorrectPriceBasedOnSize(Size size, double price)
         {
+            MadOtarGrits grits = new MadOtarGrits();
+            grits.Size = size;
+            Assert.Equal(price, grits.Price);
         }
 
         [Theory]
+        /// <summary>
+        /// small should have 105 cals
+        /// </summary>
         [InlineData(Size.Small, 105)]
+        /// <summary>
+        /// medium should have 142 cals
+        /// </summary>
         [InlineData(Size.Medium, 142)]
+        /// <summary>
+        /// large should have 179 cals
+        /// </summary>
         [InlineData(Size.Large, 179)]
         public void ShouldReturnCorrectCaloriesBasedOnSize(Size size, uint calories)
         {
+            MadOtarGrits grits = new MadOtarGrits();
+            grits.Size = size;
+            Assert.Equal(calories, grits.Calories);
         }
 
         [Theory]
+        /// <summary>
+        /// Small to string return
+        /// </summary>
         [InlineData(Size.Small, "Small Mad Otar Grits")]
+        /// <summary>
+        /// Medium to string return
+        /// </summary>
         [InlineData(Size.Medium, "Medium Mad Otar Grits")]
+        /// <summary>
+        /// Large to string return
+        /// </summary>
         [InlineData(Size.Large, "Large Mad Otar Grits")]
         public void ShouldReturnCorrectToStringBasedOnSize(Size size, string name)
         {
+            MadOtarGrits grits = new MadOtarGrits();
+            grits.Size = size;
+            Assert.Equal(name, grits.ToString());
         }
     }
 }
