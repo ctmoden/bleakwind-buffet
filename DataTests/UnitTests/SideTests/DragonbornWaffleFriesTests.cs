beb8ac561a83a48916342ae3d4d6e3796a3a99f3
Christian Moden <ctmoden@ksu.edu>
2020-09-04 21:46:19 -0500

    waffle and miraak done

--- DataTests/UnitTests/SideTests/DragonbornWaffleFriesTests.cs
+++ DataTests/UnitTests/SideTests/DragonbornWaffleFriesTests.cs
@@ -1,5 +1,6 @@
 ﻿/*
  * Author: Zachery Brunner
+ * Modified by: Christian Moden
  * Class: DragonbornWaffleFriesTests.cs
  * Purpose: Test the DragonbornWaffleFries.cs class in the Data library
  */
@@ -7,6 +8,7 @@ using Xunit;
 
 using BleakwindBuffet.Data;
 using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data.Sides;
 
 namespace BleakwindBuffet.DataTests.UnitTests.SideTests
 {
@@ -15,40 +17,90 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [Fact]
         public void ShouldBeSmallByDefault()
         {
+            DragonbornWaffleFries fries = new DragonbornWaffleFries();
+            Assert.Equal(Size.Small, fries.Size);
         }
 
         [Fact]
         public void ShouldBeAbleToSetSize()
         {
+            DragonbornWaffleFries fries = new DragonbornWaffleFries();
+            fries.Size = Size.Small;
+            Assert.Equal(Size.Small, fries.Size);
+            fries.Size = Size.Medium;
+            Assert.Equal(Size.Medium, fries.Size);
+            fries.Size = Size.Large;
+            Assert.Equal(Size.Large, fries.Size);
+            fries.Size = Size.Small;
+            Assert.Equal(Size.Small, fries.Size);
         }
 
         [Fact]
         public void ShouldReturnCorrectSpecialInstructions()
         {
+            DragonbornWaffleFries fries = new DragonbornWaffleFries();
+            Assert.Empty(fries.SpecialInstructions);
         }
 
         [Theory]
+        /// <summary>
+        /// small should cost .42
+        /// </summary>
         [InlineData(Size.Small, 0.42)]
+        /// <summary>
+        /// medium should cost .96
+        /// </summary>
         [InlineData(Size.Medium, 0.76)]
+        /// <summary>
+        /// large should cost .96
+        /// </summary>
         [InlineData(Size.Large, 0.96)]
         public void ShouldReturnCorrectPriceBasedOnSize(Size size, double price)
         {
+            DragonbornWaffleFries fries = new DragonbornWaffleFries();
+            fries.Size = size;
+            Assert.Equal(price, fries.Price);
         }
 
         [Theory]
+        /// <summary>
+        /// small should have 77 calories
+        /// </summary>
         [InlineData(Size.Small, 77)]
+        /// <summary>
+        /// medium should have 89 calories
+        /// </summary>
         [InlineData(Size.Medium, 89)]
+        /// <summary>
+        /// large should have 100 calories
+        /// </summary>
         [InlineData(Size.Large, 100)]
         public void ShouldReturnCorrectCaloriesBasedOnSize(Size size, uint calories)
         {
+            DragonbornWaffleFries fries = new DragonbornWaffleFries();
+            fries.Size = size;
+            Assert.Equal(calories, fries.Calories);
         }
 
         [Theory]
+        /// <summary>
+        /// Small to string return
+        /// </summary>
         [InlineData(Size.Small, "Small Dragonborn Waffle Fries")]
+        /// <summary>
+        /// medium to string return
+        /// </summary>
         [InlineData(Size.Medium, "Medium Dragonborn Waffle Fries")]
+        /// <summary>
+        /// Large to string return
+        /// </summary>
         [InlineData(Size.Large, "Large Dragonborn Waffle Fries")]
         public void ShouldReturnCorrectToStringBasedOnSize(Size size, string name)
         {
+            DragonbornWaffleFries fries = new DragonbornWaffleFries();
+            fries.Size = size;
+            Assert.Equal(name, fries.ToString());
+
         }
     }
 }
--- DataTests/UnitTests/SideTests/FriedMirrakTests.cs
+++ DataTests/UnitTests/SideTests/FriedMirrakTests.cs
@@ -1,5 +1,6 @@
 ﻿/*
  * Author: Zachery Brunner
+ * Modified by: Christian Moden
  * Class: FriedMiraakTests.cs
  * Purpose: Test the FriedMiraak.cs class in the Data library
  */
@@ -7,6 +8,7 @@ using Xunit;
 
 using BleakwindBuffet.Data;
 using BleakwindBuffet.Data.Enums;
+using BleakwindBuffet.Data.Sides;
 
 namespace BleakwindBuffet.DataTests.UnitTests.SideTests
 {
@@ -15,40 +17,89 @@ namespace BleakwindBuffet.DataTests.UnitTests.SideTests
         [Fact]
         public void ShouldBeSmallByDefault()
         {
+            FriedMiraak hash = new FriedMiraak();
+            Assert.Equal(Size.Small, hash.Size);
         }
 
         [Fact]
         public void ShouldBeAbleToSetSize()
         {
+            FriedMiraak hash = new FriedMiraak();
+            hash.Size = Size.Small;
+            Assert.Equal(Size.Small, hash.Size);
+            hash.Size = Size.Medium;
+            Assert.Equal(Size.Medium, hash.Size);
+            hash.Size = Size.Large;
+            Assert.Equal(Size.Large, hash.Size);
+            hash.Size = Size.Small;
+            Assert.Equal(Size.Small, hash.Size);
         }
 
         [Fact]
         public void ShouldReturnCorrectSpecialInstructions()
         {
+            FriedMiraak hash = new FriedMiraak();
+            Assert.Empty(hash.SpecialInstructions);
         }
 
         [Theory]
+        /// <summary>
+        /// small should cost 1.78
+        /// </summary>
         [InlineData(Size.Small, 1.78)]
+        /// <summary>
+        /// medium should cost 2.01
+        /// </summary>
         [InlineData(Size.Medium, 2.01)]
+        /// <summary>
+        /// large should cost 2.88
+        /// </summary>
         [InlineData(Size.Large, 2.88)]
         public void ShouldReturnCorrectPriceBasedOnSize(Size size, double price)
         {
+            FriedMiraak hash = new FriedMiraak();
+            hash.Size = size;
+            Assert.Equal(price, hash.Price);
         }
 
         [Theory]
+        /// <summary>
+        /// small should have 151 cals
+        /// </summary>
         [InlineData(Size.Small, 151)]
+        /// <summary>
+        /// medium should have 236 cals
+        /// </summary>
         [InlineData(Size.Medium, 236)]
+        /// <summary>
+        /// large should have 306 cals
+        /// </summary>
         [InlineData(Size.Large, 306)]
         public void ShouldReturnCorrectCaloriesBasedOnSize(Size size, uint calories)
         {
+            FriedMiraak hash = new FriedMiraak();
+            hash.Size = size;
+            Assert.Equal(calories, hash.Calories);
         }
 
         [Theory]
+        /// <summary>
+        /// Small to string return
+        /// </summary>
         [InlineData(Size.Small, "Small Fried Miraak")]
+        /// <summary>
+        /// Medium to string return
+        /// </summary>
         [InlineData(Size.Medium, "Medium Fried Miraak")]
+        /// <summary>
+        /// Large to string return
+        /// </summary>
         [InlineData(Size.Large, "Large Fried Miraak")]
         public void ShouldReturnCorrectToStringBasedOnSize(Size size, string name)
         {
+            FriedMiraak hash = new FriedMiraak();
+            hash.Size = size;
+            Assert.Equal(name, hash.ToString());
         }
     }
 }
