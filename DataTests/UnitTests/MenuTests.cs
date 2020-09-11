7e7e414f67d18ac3dc023b237b8dcd3c9378f16c
Christian Moden <ctmoden@ksu.edu>
2020-09-11 16:22:33 -0500

    REEEEEEEE

--- DataTests/UnitTests/MenuTests.cs
+++ DataTests/UnitTests/MenuTests.cs
@@ -26,7 +26,17 @@ namespace BleakwindBuffet.DataTests.MenuTests
         public void EntreesMethodShouldReturnIEnumerable()
         {
             //create another method for checking list length
-            
+            IEnumerable<IOrderItem> entrees = Menu.Entrees();
+            Assert.Collection(entrees,
+                item=> item.ToString().Contains("Briarheart Burger"),
+                item => item.ToString().Contains("Double Draugr"),
+                item => item.ToString().Contains("Garden Orc Omelette"),
+                item => item.ToString().Contains("Philly Poacher"),
+                item => item.ToString().Contains("Smokehouse Skeleton"),
+                item => item.ToString().Contains("Thalmor Triple"),
+                item => item.ToString().Contains("Thugs T-Bone")
+
+                );
 
                 
 
@@ -62,7 +72,33 @@ namespace BleakwindBuffet.DataTests.MenuTests
                 item=> item.ToString().Contains("Small Aretino Apple Juice"),
                 item => item.ToString().Contains("Medium Aretino Apple Juice"),
                 item => item.ToString().Contains("Large Aretino Apple Juice"),
-
+                item => item.ToString().Contains("Small Candlehearth Coffee"),
+                item => item.ToString().Contains("Medium Candlehearth Coffee"),
+                item => item.ToString().Contains("Large Candlehearth Coffee"),
+                item => item.ToString().Contains("Small Markarth Milk"),
+                item => item.ToString().Contains("Medium Markarth Milk"),
+                item => item.ToString().Contains("Large Markarth Milk"),
+                item => item.ToString().Contains("Small Blackberry Sailor Soda"),
+                item => item.ToString().Contains("Medium Blackberry Sailor Soda"),
+                item => item.ToString().Contains("Large Blackberry Sailor Soda"),
+                item => item.ToString().Contains("Small Cherry Sailor Soda"),
+                item => item.ToString().Contains("Medium Cherry Sailor Soda"),
+                item => item.ToString().Contains("Large Cherry Sailor Soda"),
+                item => item.ToString().Contains("Small Grapefruit Sailor Soda"),
+                item => item.ToString().Contains("Medium Grapefruit Sailor Soda"),
+                item => item.ToString().Contains("Large Grapefruit Sailor Soda"),
+                item => item.ToString().Contains("Small Lemon Sailor Soda"),
+                item => item.ToString().Contains("Medium Lemon Sailor Soda"),
+                item => item.ToString().Contains("Large Lemon Sailor Soda"),
+                item => item.ToString().Contains("Small Peach Sailor Soda"),
+                item => item.ToString().Contains("Medium Peach Sailor Soda"),
+                item => item.ToString().Contains("Large Peach Sailor Soda"),
+                item => item.ToString().Contains("Small Watermelon Sailor Soda"),
+                item => item.ToString().Contains("Medium Watermelon Sailor Soda"),
+                item => item.ToString().Contains("Large Watermelon Sailor Soda"),
+                item => item.ToString().Contains("Small Warrior Water"),
+                item => item.ToString().Contains("Medium Warrior Water"),
+                item => item.ToString().Contains("Large Warrior Water")
                 );
             
             
@@ -70,6 +106,7 @@ namespace BleakwindBuffet.DataTests.MenuTests
         [Fact]
         public void MenuShouldReturnIEnumerable()
         {
+
 
         }
     }
