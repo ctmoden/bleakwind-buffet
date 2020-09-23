167bcf2664609917e3f120b7e5d5578152df9ccc
Christian Moden <ctmoden@ksu.edu>
2020-09-23 15:29:07 -0500

    working on menu selection

---
+++ PointOfSale/MenuSelection.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for MenuSelection.xaml
+    /// </summary>
+    public partial class MenuSelection : UserControl
+    {
+        public MenuSelection()
+        {
+            InitializeComponent();
+        }
+        /// <summary>
+        /// Button event for briarheart
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">dparameters of event</param>
+        private void SelectBriarheartBurger(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">dparameters of event</param>
+        private void SelectDoubleDraugr(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+
+    }
+}
