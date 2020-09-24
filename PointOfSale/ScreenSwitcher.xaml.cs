be40b53fdf894255187364e0c56bd8deca77261c
Christian Moden <ctmoden@ksu.edu>
2020-09-24 14:40:04 -0500

    customization screens will not pop up correctly

---
+++ PointOfSale/ScreenSwitcher.xaml.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace PointOfSale
+{
+    /// <summary>
+    /// Interaction logic for ScreenSwitcher.xaml
+    /// </summary>
+    public partial class ScreenSwitcher : UserControl
+    {
+        BriarhearthCustomization briarhearthScreen = new BriarhearthCustomization();
+        MenuSelection menu = new MenuSelection();
+        public ScreenSwitcher()
+        {
+            InitializeComponent();
+            containerBorder.Child = menu;
+        }
+        /// <summary>
+        /// Button event for briarheart
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectBriarheartBurger(object sender, RoutedEventArgs e)
+        {
+            ClearPreviousScreen();
+            //containerBorder.Child = briarhearthScreen;
+            menuControls.Children.Add(briarhearthScreen);
+
+        }
+        private void ClearPreviousScreen()
+        {
+            menuControls.Children.Clear();
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectDoubleDraugr(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectThalmorTriple(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectPhillyPoacher(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectGardenOrcOmelette(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectSmokeHouse(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectThugsTBone(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectWaffleFries(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectFriedMiraak(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectGrits(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Button event for vokun salad
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectSalad(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Button event for vokun salad
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectAppleJuice(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Button event for vokun salad
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectCoffee(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Button event for markarth milk
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectMilk(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Button event for sailor soda
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectSoda(object sender, RoutedEventArgs e)
+        {
+
+        }
+        /// <summary>
+        /// Button event for sailor soda
+        /// </summary>
+        /// <param name="sender">object that sent event we are handling</param>
+        /// <param name="e">parameters of event</param>
+        private void SelectWater(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
