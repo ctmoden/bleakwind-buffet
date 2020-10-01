ed7dc61552c55f38ddc95a75047bba9b48ed4d03
Christian Moden <ctmoden@ksu.edu>
2020-09-30 22:09:05 -0500

    double done (gonna be a looooong week)

--- Data/Entree/DoubleDraugr.cs
+++ Data/Entree/DoubleDraugr.cs
@@ -7,11 +7,20 @@ using System;
 using System.Collections.Generic;
 using System.Dynamic;
 using System.Text;
-
+using System.ComponentModel;
 namespace BleakwindBuffet.Data.Entrees
 {
-    public class DoubleDraugr : Entree
+    public class DoubleDraugr : Entree, INotifyPropertyChanged
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+        /// <summary>
+        /// Invokes the property changed event handler for a property
+        /// </summary>
+        /// <param name="propertyName">name of property that just changed</param>
+        public void InvokePropertyChange(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
         /// <summary>
         /// Price property for the Double Draugr.  Gets price for the burger  
         /// Uses the shorthand method for a property
@@ -21,38 +30,110 @@ namespace BleakwindBuffet.Data.Entrees
         /// Gets the whopping amount of calories packed in the burger 
         /// </summary>
         public override uint Calories => 843;
+        private bool bun = true;
         /// <summary>
         /// getter/setter for Bun Property
         /// </summary>
-        public bool Bun { get; set; } = true;
+        public bool Bun
+        {
+            get => bun;
+            set
+            {
+                bun = value;
+                InvokePropertyChange("Bun");
+            }
+        }
+        private bool ketchup = true;
         /// <summary>
         /// getter/setter for Ketchup property
         /// </summary>
-        public bool Ketchup { get; set; } = true;
+        public bool Ketchup
+        {
+            get => ketchup;
+            set
+            {
+                ketchup = value;
+                InvokePropertyChange("Ketchup");
+            }
+        }
+        private bool mustard = true;
         /// <summary>
         /// getter/setter for Mustard property
         /// </summary>
-        public bool Mustard { get; set; } = true;
+        public bool Mustard
+        {
+            get => mustard;
+            set
+            {
+                mustard = value;
+                InvokePropertyChange("Mustard");
+            }
+        }
+        private bool pickle = true;
         /// <summary>
         /// getter/setter for Pickle property
         /// </summary>
-        public bool Pickle { get; set; } = true;
+        public bool Pickle
+        {
+            get => pickle;
+            set
+            {
+                pickle = value;
+                InvokePropertyChange("Pickle");
+            }
+        }
+        private bool cheese = true;
         /// <summary>
         /// getter/setter for Cheese property
         /// </summary>
-        public bool Cheese { get; set; } = true;
+        public bool Cheese
+        {
+            get => cheese;
+            set
+            {
+                cheese = value;
+                InvokePropertyChange("Cheese");
+            }
+        }
+        private bool tomato = true;
         /// <summary>
         /// property with a getter/setter for Tomato.
         /// </summary>
-        public bool Tomato { get; set; } = true;
+        public bool Tomato
+        {
+            get => tomato;
+            set
+            {
+                tomato = value;
+                InvokePropertyChange("Tomato");
+            }
+        }
+        private bool lettuce = true;
         /// <summary>
         /// property with a bool getter/setter for the Lettuce.
         /// </summary>
-        public bool Lettuce { get; set; } = true;
+        public bool Lettuce
+        {
+            get => lettuce;
+            set
+            {
+                lettuce = value;
+                InvokePropertyChange("Lettuce");
+            }
+        }
+        private bool mayo = true;
         /// <summary>
         /// property with a getter/setter for Mayo
         /// </summary>
-        public bool Mayo { get; set; } = true;
+        public bool Mayo
+        {
+            get => mayo;
+            set
+            {
+                mayo = value;
+                InvokePropertyChange("Mayo");
+            }
+        }
         /// <summary>
         ///List of special instructions specific to the Double Draugr burger.
         ///Updates the list based on property states, creates a new list with each update.
@@ -62,14 +143,46 @@ namespace BleakwindBuffet.Data.Entrees
             get
             {
                 List<string> specialInstructions = new List<string>();
-                if (!Bun) specialInstructions.Add("Hold bun");
-                if (!Ketchup) specialInstructions.Add("Hold ketchup");
-                if (!Mustard) specialInstructions.Add("Hold mustard");
-                if (!Pickle) specialInstructions.Add("Hold pickle");
-                if (!Cheese) specialInstructions.Add("Hold cheese");
-                if (!Tomato) specialInstructions.Add("Hold tomato");
-                if (!Lettuce) specialInstructions.Add("Hold lettuce");
-                if (!Mayo) specialInstructions.Add("Hold mayo");
+                if (!Bun)
+                {
+                    specialInstructions.Add("Hold bun");
+                    InvokePropertyChange("Special Instructions");
+                }
+                if (!Ketchup)
+                {
+                    specialInstructions.Add("Hold ketchup");
+                    InvokePropertyChange("Special Instructions");
+                }
+                if (!Mustard)
+                {
+                    specialInstructions.Add("Hold mustard");
+                    InvokePropertyChange("Special Instructions");
+                }
+                if (!Pickle)
+                {
+                    specialInstructions.Add("Hold pickle");
+                    InvokePropertyChange("Special Instructions");
+                }
+                if (!Cheese)
+                {
+                    specialInstructions.Add("Hold cheese");
+                    InvokePropertyChange("Special Instructions");
+                }
+                if (!Tomato)
+                {
+                    specialInstructions.Add("Hold tomato");
+                    InvokePropertyChange("Special Instructions");
+                }
+                if (!Lettuce)
+                {
+                    specialInstructions.Add("Hold lettuce");
+                    InvokePropertyChange("Special Instructions");
+                }
+                if (!Mayo)
+                {
+                    specialInstructions.Add("Hold mayo");
+                    InvokePropertyChange("Special Instructions");
+                }
                 return specialInstructions;
             }
             //FIXME do we need a setter?  I think it is fine on first guess.
