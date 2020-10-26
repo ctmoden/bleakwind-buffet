b6f946c4d44f1bff171ee719d15a2de5b12e9c92
Christian Moden <ctmoden@ksu.edu>
2020-10-26 11:01:54 -0500

    still have to fix Layout Title, but have a framework for index and moving onto css ops.  Site won't actually pop up

--- Website/Pages/Index.cshtml.cs
+++ Website/Pages/Index.cshtml.cs
@@ -6,6 +6,7 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
 using BleakwindBuffet.Data.Entrees;
+using BleakwindBuffet.Data.Enums;
 
 namespace Website.Pages
 {
